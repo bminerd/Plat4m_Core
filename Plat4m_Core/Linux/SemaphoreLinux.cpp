b1278934140ec0780710e440f1bf5ae2cf8c0899
Ben Minerd <ben.minerd@gmail.com>
2021-08-26 15:32:35 -0400

    42: Added Linux Semaphore implementation.

---
+++ Plat4m_Core/Linux/SemaphoreLinux.cpp
@@ -0,0 +1,110 @@
+//------------------------------------------------------------------------------
+//       _______    __                           ___
+//      ||  ___ \  || |             __          //  |
+//      || |  || | || |   _______  || |__      //   |    _____  ___
+//      || |__|| | || |  // ___  | ||  __|    // _  |   ||  _ \/ _ \
+//      ||  ____/  || | || |  || | || |      // /|| |   || |\\  /\\ \
+//      || |       || | || |__|| | || |     // /_|| |_  || | || | || |
+//      || |       || |  \\____  | || |__  //_____   _| || | || | || |
+//      ||_|       ||_|       ||_|  \\___|       ||_|   ||_| ||_| ||_|
+//
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2021 Benjamin Minerd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
+
+///
+/// @file SemaphoreLinux.cpp
+/// @author Ben Minerd
+/// @date 8/19/2021
+/// @brief SemaphoreLinux class source file.
+///
+
+//------------------------------------------------------------------------------
+// Include files
+//------------------------------------------------------------------------------
+
+#include <Plat4m_Core/Linux/SemaphoreLinux.h>
+#include <Plat4m_Core/Plat4m.h>
+
+using namespace std;
+using namespace Plat4m;
+
+//------------------------------------------------------------------------------
+// Public constructors
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+SemaphoreLinux::SemaphoreLinux(const uint32_t maxValue,
+                               const uint32_t initialValue) :
+    Semaphore(maxValue, initialValue),
+    mySemaphoreHandle()
+{
+    int returnValue = sem_init(&mySemaphoreHandle, 0, initialValue);
+
+    if (returnValue != 0)
+    {
+        while (true)
+        {
+            // Lock up, unable to create semaphore
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+// Public destructors
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+SemaphoreLinux::~SemaphoreLinux()
+{
+}
+
+//------------------------------------------------------------------------------
+// Private virtual methods implemented overridden for Semaphore
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+Semaphore::Error SemaphoreLinux::driverWait()
+{
+	sem_wait(&mySemaphoreHandle);
+
+    return Error(ERROR_CODE_NONE);
+}
+
+//------------------------------------------------------------------------------
+Semaphore::Error SemaphoreLinux::driverPost()
+{
+	sem_post(&mySemaphoreHandle);
+
+    return Error(ERROR_CODE_NONE);
+}
+
+//------------------------------------------------------------------------------
+uint32_t SemaphoreLinux::driverGetValue()
+{
+	uint32_t value;
+
+    sem_getvalue(&mySemaphoreHandle, (int*) &value);
+
+    return value;
+}
