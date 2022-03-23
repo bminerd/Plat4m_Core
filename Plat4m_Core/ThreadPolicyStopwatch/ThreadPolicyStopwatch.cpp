05229dd18a126aaac71edb6b52df56395a3594eb
Ben Minerd <ben.minerd@gmail.com>
2022-03-23 11:57:05 -0400

    66: Added ThreadPolicy and ThreadPolicyManager implementations for a Stopwatch policy.

---
+++ Plat4m_Core/ThreadPolicyStopwatch/ThreadPolicyStopwatch.cpp
@@ -0,0 +1,83 @@
+//------------------------------------------------------------------------------
+//       _______    __                           ___
+//      ||  ___ \  || |             __          //  |
+//      || |  || | || |   _______  || |__      //   |    _____  ___
+//      || |__|| | || |  // ___  | ||  __|    // _  |   ||  _ \/ _ \
+//      ||  ____/  || | || |  || | || |      // /|| |   || |\\  /\\ \
+//      || |       || | || |__|| | || |     // /_|| |_  || | || | || |
+//      || |       || |  \\____  | || |__  //_____   _| || | || | || |
+//      ||_|       ||_|       ||_|  \\___|       ||_|   ||_| ||_| ||_|
+//
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Benjamin Minerd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
+
+///
+/// @file ThreadPolicyStopwatch.h
+/// @author Ben Minerd
+/// @date 3/1/2022
+/// @brief ThreadPolicyStopwatch class source file.
+///
+
+//------------------------------------------------------------------------------
+// Include files
+//------------------------------------------------------------------------------
+
+#include <Plat4m_Core/ThreadPolicyStopwatch/ThreadPolicyStopwatch.h>
+#include <Plat4m_Core/StopwatchManager.h>
+
+using namespace Plat4m;
+
+//------------------------------------------------------------------------------
+// Public constructors
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+ThreadPolicyStopwatch::ThreadPolicyStopwatch() :
+    ThreadPolicy(),
+    myStopwatch(StopwatchManager::createStopwatch())
+{
+}
+
+//------------------------------------------------------------------------------
+// Protected virtual destructors
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+ThreadPolicyStopwatch::~ThreadPolicyStopwatch()
+{
+}
+
+//------------------------------------------------------------------------------
+// Public virtual methods overridden for ThreadPolicy
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+void ThreadPolicyStopwatch::apply(Thread& thread, Callback<>& threadCallback)
+{
+    myStopwatch.start();
+
+    threadCallback.call();
+
+    myStopwatch.stop();
+}
