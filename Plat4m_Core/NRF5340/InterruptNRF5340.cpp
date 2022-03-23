e92ffd19b2d45091bed3d5ca025b2f02a18fbb17
Ben Minerd <ben.minerd@gmail.com>
2022-03-23 16:54:22 -0400

    71: Updated InterruptNRF5340 driver.

--- Plat4m_Core/NRF5340/InterruptNRF5340.cpp
+++ Plat4m_Core/NRF5340/InterruptNRF5340.cpp
@@ -1,6 +1,36 @@
-/**
- * Copyright (c) 2021, SignalQuest LLC
- */
+//------------------------------------------------------------------------------
+//       _______    __                           ___
+//      ||  ___ \  || |             __          //  |
+//      || |  || | || |   _______  || |__      //   |    _____  ___
+//      || |__|| | || |  // ___  | ||  __|    // _  |   ||  _ \/ _ \
+//      ||  ____/  || | || |  || | || |      // /|| |   || |\\  /\\ \
+//      || |       || | || |__|| | || |     // /_|| |_  || | || | || |
+//      || |       || |  \\____  | || |__  //_____   _| || | || | || |
+//      ||_|       ||_|       ||_|  \\___|       ||_|   ||_| ||_| ||_|
+//
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Benjamin Minerd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
 
 ///
 /// @file InterruptNRF5340.cpp
@@ -47,20 +77,26 @@ const IRQn_Type InterruptNRF5340::myInterruptMap[] =
     PendSV_IRQn,
     /// ID_SYS_TICK
     SysTick_IRQn,
-    /// ID_POWER_CLOCK,
-    POWER_CLOCK_IRQn,
-    /// ID_RADIO
-    RADIO_IRQn,
-    /// ID_UARTE0_UART0
-    UARTE0_UART0_IRQn,
-    /// ID_SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0
-    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0_IRQn,
-    /// ID_SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1
-    SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn,
-    /// ID_NFCT
-    NFCT_IRQn,
-    /// ID_GPIOTE
-    GPIOTE_IRQn,
+    /// ID_FPU
+    FPU_IRQn,
+    /// ID_CAHCE
+    CACHE_IRQn,
+    /// ID_SPU
+    SPU_IRQn,
+    /// ID_CLOCK_POWER
+    CLOCK_POWER_IRQn,
+    /// ID_SPIM0_SPIS0_TWIM0_TWIS0_UARTE0
+    SPIM0_SPIS0_TWIM0_TWIS0_UARTE0_IRQn,
+    /// ID_SPIM1_SPIS1_TWIM1_TWIS1_UARTE1
+    SPIM1_SPIS1_TWIM1_TWIS1_UARTE1_IRQn,
+    /// ID_SPIM4
+    SPIM4_IRQn,
+    /// ID_SPIM2_SPIS2_TWIM2_TWIS2_UARTE2
+    SPIM2_SPIS2_TWIM2_TWIS2_UARTE2_IRQn,
+    /// ID_SPIM3_SPIS3_TWIM3_TWIS3_UARTE3
+    SPIM3_SPIS3_TWIM3_TWIS3_UARTE3_IRQn,
+    /// ID_GPIOTE0
+    GPIOTE0_IRQn,
     /// ID_SAADC
     SAADC_IRQn,
     /// ID_TIMER0
@@ -71,68 +107,58 @@ const IRQn_Type InterruptNRF5340::myInterruptMap[] =
     TIMER2_IRQn,
     /// ID_RTC0
     RTC0_IRQn,
-    /// ID_TEMP
-    TEMP_IRQn,
-    /// ID_RNG
-    RNG_IRQn,
-    /// ID_ECB
-    ECB_IRQn,
-    /// ID_CCM_AAR
-    CCM_AAR_IRQn,
-    /// ID_WDT
-    WDT_IRQn,
     /// ID_RTC1
     RTC1_IRQn,
-    /// ID_QDEC,
-    QDEC_IRQn,
+    /// ID_WDT0
+    WDT0_IRQn,
+    /// ID_WDT1
+    WDT1_IRQn,
     /// ID_COMP_LPCOMP
     COMP_LPCOMP_IRQn,
-    /// ID_SWI0_EGU0
-    SWI0_EGU0_IRQn,
-    /// ID_SWI1_EGU1
-    SWI1_EGU1_IRQn,
-    /// ID_SWI2_EGU2
-    SWI2_EGU2_IRQn,
-    /// ID_SWI3_EGU3
-    SWI3_EGU3_IRQn,
-    /// ID_SWI4_EGU4
-    SWI4_EGU4_IRQn,
-    /// ID_SWI5_EGU5
-    SWI5_EGU5_IRQn,
-    /// ID_TIMER3
-    TIMER3_IRQn,
-    /// ID_TIMER4
-    TIMER4_IRQn,
+    /// ID_EGU0
+    EGU0_IRQn,
+    /// ID_EGU1
+    EGU1_IRQn,
+    /// ID_EGU2
+    EGU2_IRQn,
+    /// ID_EGU3
+    EGU3_IRQn,
+    /// ID_EGU4
+    EGU4_IRQn,
+    /// ID_EGU5
+    EGU5_IRQn,
     /// ID_PWM0
     PWM0_IRQn,
-    /// ID_PDM
-    PDM_IRQn,
-    /// ID_MWU
-    MWU_IRQn,
     /// ID_PWM1
     PWM1_IRQn,
     /// ID_PWM2
     PWM2_IRQn,
-    /// ID_SPIM2_SPIS2_SPI2
-    SPIM2_SPIS2_SPI2_IRQn,
-    /// ID_RTC2
-    RTC2_IRQn,
-    /// ID_I2S
-    I2S_IRQn,
-    /// ID_FPU
-    FPU_IRQn,
-    /// ID_USBD
-    USBD_IRQn,
-    /// ID_UARTE1
-    UARTE1_IRQn,
-    /// ID_QSPI
-    QSPI_IRQn,
-    /// ID_CRYPTOCELL
-    CRYPTOCELL_IRQn,
     /// ID_PWM3
     PWM3_IRQn,
-    /// ID_SPIM3
-    SPIM3_IRQn
+    /// ID_PDM0
+    PDM0_IRQn,
+    /// ID_I2S0
+    I2S0_IRQn,
+    /// ID_IPC
+    IPC_IRQn,
+    /// ID_QSPI
+    QSPI_IRQn,
+    /// ID_NFCT
+    NFCT_IRQn,
+    /// ID_GPIOTE1
+    GPIOTE1_IRQn,
+    /// ID_QDEC0
+    QDEC0_IRQn,
+    /// ID_QDEC1
+    QDEC1_IRQn,
+    /// ID_USBD
+    USBD_IRQn,
+    /// ID_USB_REGULATOR
+    USBREGULATOR_IRQn,
+    /// ID_KMU
+    KMU_IRQn,
+    /// ID_CRYPTOCEL
+    CRYPTOCELL_IRQn
 };
 
 //------------------------------------------------------------------------------
@@ -141,7 +167,7 @@ const IRQn_Type InterruptNRF5340::myInterruptMap[] =
 
 //------------------------------------------------------------------------------
 InterruptNRF5340::InterruptNRF5340(const Id id,
-                                     HandlerCallback& handlerCallback) :
+                                   HandlerCallback& handlerCallback) :
     Interrupt(handlerCallback),
     myId(id)
 {
@@ -156,27 +182,11 @@ Module::Error InterruptNRF5340::driverSetEnabled(const bool enabled)
 {
     if (enabled)
     {
-        if (ProcessorNRF5340::isSoftDevicePresent())
-        {
-            sd_nvic_EnableIRQ(myInterruptMap[myId]);
-        }
-        else
-        {
-            NVIC_EnableIRQ(myInterruptMap[myId]);
-        }
-
+        NVIC_EnableIRQ(myInterruptMap[myId]);
     }
     else
     {
-        if (ProcessorNRF5340::isSoftDevicePresent())
-        {
-            sd_nvic_DisableIRQ(myInterruptMap[myId]);
-        }
-        else
-        {
-            NVIC_DisableIRQ(myInterruptMap[myId]);
-        }
-        
+        NVIC_DisableIRQ(myInterruptMap[myId]);
     }
 
     return Module::Error(Module::ERROR_CODE_NONE);
@@ -189,14 +199,7 @@ Module::Error InterruptNRF5340::driverSetEnabled(const bool enabled)
 //------------------------------------------------------------------------------
 Interrupt::Error InterruptNRF5340::driverConfigure(const Config& config)
 {
-    if (ProcessorNRF5340::isSoftDevicePresent())
-    {
-        sd_nvic_SetPriority(myInterruptMap[myId], config.priority);
-    }
-    else
-    {
-        NVIC_SetPriority(myInterruptMap[myId], config.priority);
-    }
+    NVIC_SetPriority(myInterruptMap[myId], config.priority);
 
     return Error(ERROR_CODE_NONE);
 }
--- Plat4m_Core/NRF5340/InterruptNRF5340.h
+++ Plat4m_Core/NRF5340/InterruptNRF5340.h
@@ -1,6 +1,36 @@
-/**
- * Copyright (c) 2021, SignalQuest LLC
- */
+//------------------------------------------------------------------------------
+//       _______    __                           ___
+//      ||  ___ \  || |             __          //  |
+//      || |  || | || |   _______  || |__      //   |    _____  ___
+//      || |__|| | || |  // ___  | ||  __|    // _  |   ||  _ \/ _ \
+//      ||  ____/  || | || |  || | || |      // /|| |   || |\\  /\\ \
+//      || |       || | || |__|| | || |     // /_|| |_  || | || | || |
+//      || |       || |  \\____  | || |__  //_____   _| || | || | || |
+//      ||_|       ||_|       ||_|  \\___|       ||_|   ||_| ||_| ||_|
+//
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Benjamin Minerd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
 
 ///
 /// @file InterruptNRF5340.h
@@ -18,8 +48,8 @@
 
 #include <nrf5340_application.h>
 
-#include <Plat4m_Core/Plat4m_Core/Plat4m.h>
-#include <Plat4m_Core/Plat4m_Core/Interrupt.h>
+#include <Plat4m_Core/Plat4m.h>
+#include <Plat4m_Core/Interrupt.h>
 
 //------------------------------------------------------------------------------
 // Namespaces
@@ -52,49 +82,47 @@ public:
         ID_DEBUG_MONITOR,
         ID_PEND_SV,
         ID_SYS_TICK,
-        ID_POWER_CLOCK,
-        ID_RADIO,
-        ID_UARTE0_UART0,
-        ID_SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0,
-        ID_SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1,
-        ID_NFCT,
-        ID_GPIOTE,
+        ID_FPU,
+        ID_CAHCE,
+        ID_SPU,
+        ID_CLOCK_POWER,
+        ID_SPIM0_SPIS0_TWIM0_TWIS0_UARTE0,
+        ID_SPIM1_SPIS1_TWIM1_TWIS1_UARTE1,
+        ID_SPIM4,
+        ID_SPIM2_SPIS2_TWIM2_TWIS2_UARTE2,
+        ID_SPIM3_SPIS3_TWIM3_TWIS3_UARTE3,
+        ID_GPIOTE0,
         ID_SAADC,
         ID_TIMER0,
         ID_TIMER1,
         ID_TIMER2,
         ID_RTC0,
-        ID_TEMP,
-        ID_RNG,
-        ID_ECB,
-        ID_CCM_AAR,
-        ID_WDT,
         ID_RTC1,
-        ID_QDEC,
+        ID_WDT0,
+        ID_WDT1,
         ID_COMP_LPCOMP,
-        ID_SWI0_EGU0,
-        ID_SWI1_EGU1,
-        ID_SWI2_EGU2,
-        ID_SWI3_EGU3,
-        ID_SWI4_EGU4,
-        ID_SWI5_EGU5,
-        ID_TIMER3,
-        ID_TIMER4,
+        ID_EGU0,
+        ID_EGU1,
+        ID_EGU2,
+        ID_EGU3,
+        ID_EGU4,
+        ID_EGU5,
         ID_PWM0,
-        ID_PDM,
-        ID_MWU,
         ID_PWM1,
         ID_PWM2,
-        ID_SPIM2_SPIS2_SPI2,
-        ID_RTC2,
-        ID_I2S,
-        ID_FPU,
-        ID_USBD,
-        ID_UARTE1,
-        ID_QSPI,
-        ID_CRYPTOCELL,
         ID_PWM3,
-        ID_SPIM3
+        ID_PDM0,
+        ID_I2S0,
+        ID_IPC,
+        ID_QSPI,
+        ID_NFCT,
+        ID_GPIOTE1,
+        ID_QDEC0,
+        ID_QDEC1,
+        ID_USBD,
+        ID_USB_REGULATOR,
+        ID_KMU,
+        ID_CRYPTOCELL
     };
 
     //--------------------------------------------------------------------------
