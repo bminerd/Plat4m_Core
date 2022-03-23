c3402bd24bc0e22953b0771c690d419b3d4e5086
Ben Minerd <ben.minerd@gmail.com>
2022-03-23 16:53:10 -0400

    71: Updated ProcessorNRF5340 driver.

--- Plat4m_Core/NRF5340/ProcessorNRF5340.cpp
+++ Plat4m_Core/NRF5340/ProcessorNRF5340.cpp
@@ -1,6 +1,36 @@
-/**
- * Copyright (c) 2021, SignalQuest LLC
- */
+//------------------------------------------------------------------------------
+//       _______    __                           ___
+//      ||  ___ \  || |             __          //  |
+//      || |  || | || |   _______  || |__      //   |    _____  ___
+//      || |__|| | || |  // ___  | ||  __|    // _  |   ||  _ \/ _ \
+//      ||  ____/  || | || |  || | || |      // /|| |   || |\\  /\\ \
+//      || |       || | || |__|| | || |     // /_|| |_  || | || | || |
+//      || |       || |  \\____  | || |__  //_____   _| || | || | || |
+//      ||_|       ||_|       ||_|  \\___|       ||_|   ||_| ||_| ||_|
+//
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2022 Benjamin Minerd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
 
 ///
 /// @file ProcessorNRF5340.cpp
@@ -78,9 +108,15 @@ ProcessorNRF5340::Error ProcessorNRF5340::setConfig(const Config& config)
 {
     if (config.disableTrustZone)
     {
+        // NRF_UICR_S->APPROTECT           = 0x50FA50FA;
+        NRF_CTRLAP_S->APPROTECT.DISABLE = 0x50FA50FA;
+
         myClock = NRF_CLOCK_NS;
 
         clearBit(NRF_SPU_S->PERIPHID[5].PERM, 4);
+        // clearBit(NRF_SPU_S->PERIPHID[8].PERM, 4);
+        clearBit(NRF_SPU_S->PERIPHID[9].PERM, 4);
+        // clearBit(NRF_SPU_S->PERIPHID[11].PERM, 4);
         clearBit(NRF_SPU_S->PERIPHID[66].PERM, 4);
         clearBits(NRF_SPU_S->GPIOPORT[0].PERM, 0xFFFFFFFF);
         clearBits(NRF_SPU_S->GPIOPORT[1].PERM, 0xFFFFFFFF);
